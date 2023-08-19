c7e1a7144a8b47532108b49996231cb39ef8e595
Tanvi Garg <tanvi.garg@act21.io>
2023-08-19 13:53:22 +0530

    second commit

--- src/oopsConcept/polymorphism/MethodOverloading.java
+++ src/oopsConcept/polymorphism/MethodOverloading.java
@@ -1,13 +1,39 @@
 package oopsConcept.polymorphism;
 
+import pojo.Customer;
+import pojo.Employee;
+
 public class MethodOverloading {
 
 	public void m1(String str) throws NullPointerException {
-
+		
 	}
 
 	public String m1(String str, Integer i) throws NumberFormatException {
 		return str;
+	}
+	
+	public static void main(String[] args) {
+		Customer c = new Customer("Tom", 20);
+		Customer c1 = new Customer("Tom", 20);
+		Employee e = new Employee();
+		Employee e2 = new Employee();
+		
+		System.out.println(e.equals(c));
+		System.out.println(e.equals(e2));
+		System.out.println(c1.equals(c));
+
+		String s1 = new String("abc");
+		String s2 = new String("abc");
+		
+		Integer i = new Integer(1);
+		Integer i1 = new Integer(1);
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(i.equals(i1));
+		
+		System.out.println("integer is"+s1==s2);
+		
 	}
 
 }
